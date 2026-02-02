4493e4d3e73f57b246bb230234befc99c8294624
Saakshi Raut <saakshiraut28@gmail.com>
2026-02-02 19:37:56 +0530

    clean: program.ts file

--- lib/program.ts
+++ lib/program.ts
@@ -1,7 +1,5 @@
-import { Program, AnchorProvider, web3 } from "@coral-xyz/anchor";
+import { Program, AnchorProvider } from "@coral-xyz/anchor";
 import idl from "./solana_private_prediction_market.json";
-import { PROGRAM_ID, RPC_ENDPOINT } from "./constants";
-import { Anchor } from "lucide-react";
 
 export function getProgram(wallet: any, connection: any) {
     const provider = new AnchorProvider(connection, wallet, {
